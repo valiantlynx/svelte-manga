64dfed22abf12a6a81409e4dfc693f1149b1fc8b
valiantlynx <gormerykombo@gmail.com>
2023-07-02 23:39:38 +0200

    layout and error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { page } from '$app/stores';
+
+    function goBack() {
+        window.history.back();
+    }
+
+</script>
+
+  
+  <main>
+    <h1>{$page.status}: {$page.error?.message}</h1>
+    <button on:click={goBack}>Go Back</button>
+  </main>
+  
+  <style>
+    main {
+      padding: 2rem;
+      text-align: center;
+    }
+  
+    h1 {
+      font-size: 2rem;
+      margin-bottom: 1rem;
+    }
+  </style>
+  
