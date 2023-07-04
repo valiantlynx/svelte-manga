0ec8638a29f68937750685fa17aba371c824e660
valiantlynx <gormerykombo@gmail.com>
2023-07-04 21:12:13 +0200

    works

--- src/routes/manga/+page.ts
+++ src/routes/manga/+page.ts
@@ -1,13 +1,20 @@
 import type { PageLoad } from "./$types";
+import axios, { Axios } from "axios";
 
 export const load = (async ({params}) => {
 
     const page = 1
     let mangas = [];
+    
     const url = import.meta.env.VITE_HOST_URL + `/manga?page=${page}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+    const response = await axios.get(url, {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
+
+    const data = await response.data
     mangas = data.mangas;
     
     return {
