5828d8e648da2dd85b3f206d37767409f5b2a658
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-09-06 02:05:49 +0200

    knapp

--- src/lib/components/LongstripReadingMode.svelte
+++ src/lib/components/LongstripReadingMode.svelte
@@ -10,11 +10,29 @@
 	}
 </script>
 
-
-
 <div class="flex flex-wrap justify-center max-w-full mx-auto">
+	<div class="w-full">
+		<!-- Images width selection between full or 3/5 -->
+		<div class="mb-4 flex justify-center space-x-4">
+			<button
+				class="px-4 py-2 rounded-lg btn btn-primary"
+				class:selected={imageWidth === 'full' ? 'bg-blue-500 text-white' : ''}
+				on:click={() => setImageWidth('full')}
+			>
+				Full
+			</button>
+
+			<button
+				class="px-4 py-2 rounded-lg btn btn-primary"
+				class:selected={imageWidth === '3/5' ? 'bg-blue-500 text-white' : ''}
+				on:click={() => setImageWidth('3/5')}
+			>
+				Medium
+			</button>
+		</div>
+	</div>
 	{#each data?.images as image}
-		<div class={imageWidth == 'full' ? 'w-full lg:w-3/5' : 'w-full lg:w-3/5'}>
+		<div class={`w-full lg:w-${imageWidth}`}>
 			<img
 				src={image.imageUrl}
 				alt={`${data.title} ${$page.params.chapterid} Page ${image.pageNumber}`}
