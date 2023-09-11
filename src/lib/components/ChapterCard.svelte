4e00cc7c8bf2c1cc08b13d0c0939a7461ad075f3
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-09-12 00:54:51 +0200

    Update ChapterCard.svelte

--- src/lib/components/ChapterCard.svelte
+++ src/lib/components/ChapterCard.svelte
@@ -5,5 +5,5 @@
 
 <div class="p-4 border rounded-lg shadow-md transition-transform hover:-translate-y-1">
 	<h2 class="text-xl font-bold mb-2">{chapter.chapterTitle}</h2>
-	<a href={url} class="inline-block px-4 py-2 btn rounded-lg duration-300"> Read Chapter </a>
+	<a href={url} class="inline-block px-4 py-2 btn rounded-lg duration-300" target="_self"> Read Chapter </a>
 </div>
