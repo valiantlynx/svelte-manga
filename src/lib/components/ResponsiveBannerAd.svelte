dd1f1b67dda8f05773f3df0f4656d2c066252588
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-09-14 01:39:38 +0200

    size ads

--- src/lib/components/ResponsiveBannerAd.svelte
+++ src/lib/components/ResponsiveBannerAd.svelte
@@ -1,4 +1,4 @@
-<div class="w-full py-2 mx-4 rounded-3xl">
+<div class="w-11/12 py-2 mx-4 rounded-3xl">
 	<script
 		async
 		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7827487944601896"
