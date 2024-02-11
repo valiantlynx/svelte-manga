6616fc2e4cc49b02d6fbd22c664781722071dbff
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-02-11 22:49:46 +0100

    del

--- apps/svelte-manga/setup.sh
+++
@@ -1,3 +0,0 @@
-git subtree add --prefix=apps/svelte-manga https://github.com/valiantlynx/svelte-manga.git master --squash
-git subtree pull --prefix=apps/svelte-manga https://github.com/valiantlynx/svelte-manga.git master --squash
-git subtree push --prefix=apps/svelte-manga https://github.com/valiantlynx/svelte-manga.git master
