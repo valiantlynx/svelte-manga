a336e05f67f37fd36fbd8932baaffe6ec427b033
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-03-09 19:19:03 +0000

    fixing errors

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -33,6 +33,6 @@
 		</div>
 	</div>
 	<div>
-		<p>Copyright © {new Date().getFullYear()} - All right reserved b {$page.data.siteName}</p>
+		<p>Copyright © {new Date().getFullYear()} - All right reserved by {$page.data.siteName}</p>
 	</div>
 </footer>
