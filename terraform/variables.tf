ed0f1d5169ec15a194a28a3eed95d0054ff0d963
valiantlynx <gormerykombo@gmail.com>
2024-03-14 18:15:11 +0100

    made the var file and uses it, not tested

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "subnet_cidr" {
     description = "Subnet CIDRS"
     type = list(string)
 }
+
+variable "cloudflare_zone_ids" {
+  type = map(string)
+  description = "Mapping of domain names to Cloudflare zone IDs"
+  default = {
+    "astromanga.com" = "b240f1f918bbc994dc53a9a9940ba321"
+    "auramanga.com" = "af9e01fbe3661f4df40b7d29aeb9aa9a"
+    // Add more domains as needed
+  }
+}
