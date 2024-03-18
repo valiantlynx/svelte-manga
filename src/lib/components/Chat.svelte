cf78084eb93bc3131b9ca7baa814ef514615a76a
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2024-03-18 23:53:29 +0100

    Update Chat.svelte

--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -1,25 +1,39 @@
-<script lang="ts">
+<script>
 	import ChatMessage from './ChatMessage.svelte';
 	import { onMount, onDestroy } from 'svelte';
-	import { pb } from '$lib/utils/api';
 	import { page } from '$app/stores';
-	import ProfileModal from './ProfileModal.svelte';
+	import { pb } from '$lib/utils/api';
 
-	let newMessage: string;
-	let messages: any[] = [];
-	let unsubscribe: () => void;
-	let scrollBottom: HTMLDivElement;
-	let lastScrollTop: number;
+	const company = $page.data.companyData;
+	let newMessage = '';
+	/**
+	 * @type {any[]}
+	 */
+	let messages = [];
+	/**
+	 * @type {() => void}
+	 */
+	let unsubscribe;
+	/**
+	 * @type {HTMLDivElement}
+	 */
+	let scrollBottom;
+	/**
+	 * @type {number}
+	 */
+	let lastScrollTop;
 	let canAutoScroll = true;
 	let unreadMessages = false;
-	let loading = false;
 
 	function autoScroll() {
 		setTimeout(() => scrollBottom?.scrollIntoView({ behavior: 'smooth' }), 50);
 		unreadMessages = false;
 	}
 
-	function watchScroll(e: any) {
+	/**
+	 * @param {any} e
+	 */
+	function watchScroll(e) {
 		canAutoScroll = (e.target.scrollTop || Infinity) > lastScrollTop;
 		lastScrollTop = e.target.scrollTop;
 		unreadMessages = !canAutoScroll;
@@ -34,7 +48,6 @@
 					: `mangaid="${$page.params.id}"`,
 				expand: 'sender'
 			});
-
 			return resultList.items;
 		} catch (error) {
 			console.error('Fetching initial messages error:', error);
@@ -42,10 +55,14 @@
 		}
 	}
 
-	async function handleRealtimeMessage({ action, record }: any) {
+	/**
+	 * @param {{ action: string; record: any; }} param0
+	 */
+	async function handleRealtimeMessage({ action, record }) {
 		try {
 			if (action === 'create') {
 				const sender = await pb.collection('users').getOne(record.sender);
+
 				record.expand = { sender };
 				messages = [...messages, record];
 
@@ -68,83 +85,47 @@
 
 	onDestroy(() => {
 		unsubscribe?.();
-		messages = [];
 	});
 
 	async function sendMessage() {
-		loading = true;
 		const data = {
+			sender: $page.data.user?.id ? $page.data.user?.id : 'cl1wlk9epnlmdsg',
 			message: newMessage,
-			sender: $page.data.user?.id,
 			chapterid: $page.params.chapterid,
 			mangaid: $page.params.id,
-			receiver: $page.data.user?.id
+			receiver: $page.data.user?.id ? $page.data.user?.id : 'vjqw7rzlxvxkr16',
 		};
 		await pb.collection('chat_animevariant').create(data);
 		newMessage = '';
 		canAutoScroll = true;
 		autoScroll();
-		loading = false;
 	}
 </script>
 
-<div class=" space-y-4  pb-4 border border-primary m-4 bg-base-200">
-	<h2 class="text-2xl font-bold mb-4 bg-primary text-primary-content rounded-b-md w-full">
-		<i class="fa fa-comments mx-2"></i> 
-		Join the Discussion
-	</h2>
+<div class="space-y-4 items-center py-8">
+	<div class="p-4 space-y-4 border-dashed border-2 border-primary">
+		<h2 class="text-2xl font-bold mb-4">Join the Discussion</h2>
+	
+		<main class="overflow-y-auto" on:scroll={watchScroll}>
+			{#each messages as message (message.id)}
+				<ChatMessage {message} sender={$page.data.user?.username} />
+			{/each}
+		</main>
 
-	<main class="overflow-y-auto max-h-[60vh] px-4" on:scroll={watchScroll}>
-		{#each messages as message (message.id)}
-			<ChatMessage {message} sender={$page.data.user?.username} />
-		{/each}
-		<div class="dummy" bind:this={scrollBottom} />
-	</main>
-
-	{#if !canAutoScroll}
-		<div class="text-center justify-center flex">
-			<button on:click={autoScroll} class="btn btn-primary">
-				{#if unreadMessages}
-					💬
+		<div class="border-t border-primary pt-4">
+			<form on:submit|preventDefault={sendMessage} class="space-x-2 flex items-center">
+				{#if !$page.data.user}
+					<a href="/login" type="submit" class="btn btn-primary">login</a>
 				{/if}
-				🡣
-			</button>
-		</div>
-	{/if}
-
-	<div class="divider px-4 "></div>
-
-	<div class="border-t border-primary pt-4	">
-		<div class="flex flex-wrap w-full">
-			<div class="left-content mb-2 ml-2 w-full lg:w-1/4  order-1 flex">
-				<ProfileModal />
-				{#if $page.data.user}
-					<div class="flex-grow ml-2 mb-2"> 
-						<h3 class="text-lg font-bold break-all">{$page.data.user?.username}</h3>
-						<p class="text-sm break-all">{$page.data.user?.email}</p>
-					</div>
-				{/if}
-			</div>
-			<div class="right-content w-full lg:w-3/4 order-2">
-				<form on:submit|preventDefault={sendMessage} class=" flex items-center">
-			
-					<input
+				<input
 					type="text"
-					placeholder={$page.data.user ? 'Type a message...' : '<------ Login to chat 💬'}
-					id="comment"
-					required
+					placeholder="Type a message..."
 					minlength="1"
 					bind:value={newMessage}
-					disabled={loading}
 					class="input input-bordered input-primary flex-grow"
-					
-					/>
-					{#if $page.data.user}
-						<button type="submit" disabled={loading} class="btn btn-primary"> Send </button>
-					{/if}
-				</form>
-			</div>
+				/>
+				<button type="submit" class="btn btn-primary">send</button>
+			</form>
 		</div>
 	</div>
 </div>
-
