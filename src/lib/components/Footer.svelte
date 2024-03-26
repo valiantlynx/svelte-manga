ee1b01525d67186fe4f4e8f576142e6cb38d42d8
valiantlynx <gormerykombo@gmail.com>
2024-03-26 18:44:05 +0100

    contact and terms of servoce

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,31 @@
 	const logo = $page.data.sites?.logo
                 ? `${site.site.pocketbase}/api/files/${$page.data.sites?.collectionId}/${$page.data.sites?.id}/${$page.data.sites?.logo}`
                 : `https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=${$page.data.sites?.siteName}`;
+    // Dynamically set or change links based on conditions
+    const links = [
+        { href: "/about", text: "About us" },
+        { href: "/contact", text: "Contact" },
+        { href: "/dmca-notice", text: "DCMA" },
+        { href: "/privacy-policy", text: "Privacy" },
+        { href: "/terms-of-service", text: "Terms of Service" }, // Fixed typo "og" to "of"
+        { href: "/sitemap.xml", text: "Sitemap" }
+    ];
 </script>
+
+
+<style>
+    @media (max-width: 768px) {
+        .responsive-grid {
+            grid-template-columns: repeat(2, minmax(0, 1fr));
+        }
+    }
+
+    @media (min-width: 769px) {
+        .responsive-grid {
+            grid-template-columns: repeat(6, minmax(0, 1fr));
+        }
+    }
+</style>
 
 <div class="footer footer-center p-10 bg-base-300 text-base-content rounded mt-auto">
 	<a href="/" class="p-1">
@@ -13,13 +37,11 @@
 				<img src={logo} alt="{$page.data.siteName} logo"  class="w-80" />
 		{/if}
 </a>
-	<div class="grid grid-flow-col gap-4">
-		<a href="/about" class="link link-hover">About us</a>
-		<a href="/contact" class="link link-hover">Contact</a>
-		<a href="/dmca-notice" class="link link-hover">DCMA</a>
-		<a href="/privacy-policy" class="link link-hover">Privacy</a>
-		<a href="/sitemap.xml" class="link link-hover">Sitemap</a>
-	</div>
+<div class="grid responsive-grid gap-4">
+    {#each links as link}
+        <a href={link.href} class="link link-hover">{link.text}</a>
+    {/each}
+</div>
 	<div>
 		<div class="grid grid-flow-col gap-4">
 			<a href="https://www.facebook.com/animevariant/" class="link link-hover hover:link-primary"
