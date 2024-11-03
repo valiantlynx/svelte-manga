0954d26e97c2e56b1541345584e0c31d319db2a5
Gormery Kombo Wanjiru <gormerykw@student.uia.no>
2024-11-03 19:50:33 +0100

    big search works too

--- src/lib/components/BigSearchResults.svelte
+++ src/lib/components/BigSearchResults.svelte
@@ -2,32 +2,29 @@
 	import { searchQuery } from '$lib/utils/stores';
 	export let searchResults: any = [];
 	export let handleClick: any;
-	let { VITE_PUBLIC_API } = import.meta.env;
-	console.log(searchResults)
-  </script>
-  
-  <ul
+	let {VITE_PUBLIC_API} = import.meta.env
+</script>
+
+<ul
 	class="bg-base-100 w-full text-base-content rounded-box overflow-y-auto -z-0"
 	hidden={$searchQuery === ''}
-  >
+>
 	{#if searchResults.length > 0}
-	  <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 w-full">
-		{#each searchResults as result}
-		  <li class="btn-ghost bg-base-300 shadow-md rounded-lg p-4 m-1">
-			<button
-			  on:click={() => handleClick(`/${result.type}/${result.id}`)}
-			  aria-label={'Go to ' + result.title + ' ' + result.type + ' page'}
-			>
-			  <h3 class="text-lg font-semibold">{result.title}</h3>
-			  <img src={VITE_PUBLIC_API + '/api' + result.img} alt={result.title} class="w-full rounded-lg mt-2" />
-			  <p class="text-sm mt-2">Authors: {result.author}</p>
-			  <p class="text-sm mt-1">Type: {result.type}</p>
-			</button>
-		  </li>
-		{/each}
-	  </div>
+		<div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 w-full">
+			{#each searchResults as result}
+				<li class="btn-ghost bg-base-300 shadow-md rounded-lg p-4 m-1">
+					<button
+						on:click={handleClick(result.src)}
+						aria-label={'Go to ' + result.title + ' manga page'}
+					>
+						<h3 class="text-lg font-semibold">{result.title}</h3>
+						<img src={result.img.startsWith('http') ? result.img : VITE_PUBLIC_API + '/api' + result.img} alt={result.title} class="w-full rounded-lg mt-2" />
+						<p class="text-sm mt-2">Authors: {result.author}</p>
+					</button>
+				</li>
+			{/each}
+		</div>
 	{:else}
-	  <p>No search results</p>
+		<p>No search results</p>
 	{/if}
-  </ul>
-  
+</ul>
